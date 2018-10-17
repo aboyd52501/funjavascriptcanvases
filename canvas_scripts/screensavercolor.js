1fd8d0681a625e69107daa0788323f3b12c29eb4
Ashton Boyd <aboyd52501@gmail.com>
2018-10-17 13:42:40 -0500

    Fixed main html file name, added smoothing and lightness/darkness modulation

--- canvas_scripts/screensavercolor.js
+++ canvas_scripts/screensavercolor.js
@@ -1,9 +1,9 @@
-var boxSizeX = s/5;
-var boxSizeY = s/5;
+var boxSizeX = s/9;
+var boxSizeY = s/9;
 
 // velocities
 var boxVelX = s/2;
-var boxVelY = s/Math.E;
+var boxVelY = s/2.435232634634;
 
 var boxPosX = w/2;
 var boxPosY = h/2;
@@ -13,18 +13,23 @@ ctx.fillRect(0,0,w,h);
 
 function drawCanvas() {
 	
-	ctx.fillStyle = 'hsl('+(time*40 % 360 )+',100%,50%)';
-	ctx.fillRect( boxPosX-boxSizeX/2, boxPosY-boxSizeY/2, boxSizeX, boxSizeY );
+	ctx.fillStyle = 'hsl('+(time*100 % 360 )+',100%,'+(Math.sin(time/5)*20+50)+'%)';
+	interpSteps = 5;
+	for( var i = 0; i<interpSteps; i++ ) {
+		ctx.fillRect( boxVelX/fps*(i/interpSteps)+boxPosX-boxSizeX/2, boxVelY/fps*(i/interpSteps)+boxPosY-boxSizeY/2, boxSizeX, boxSizeY );
+	}
 	
 	boxPosX += boxVelX/fps;
 	boxPosY += boxVelY/fps;
 	
+	// Bounce off walls
+	// X
 	if( boxPosX + boxSizeX/2 >= w ) {
 		boxVelX *= -1;
 	} else if ( boxPosX - boxSizeX/2 <= 0 ) {
 		boxVelX *= -1;
 	}
-	
+	// Y
 	if( boxPosY + boxSizeY/2 >= h ) {
 		boxVelY *= -1;
 	} else if ( boxPosY - boxSizeY/2 <= 0 ) {
